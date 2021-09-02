3c2fcda5c91af24d0bfbf752c9f47bdf9d394de2
Fabricio Carneiro Piscinelli <58093013+Fabricio-Carneiro-Piscinelli@users.noreply.github.com>
2021-09-02 19:57:00 -0300

    feat: [00004]: config modules

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-const express = require('express');
+
+// const express = require('express');
+import express from 'express';
 const app = express();
 const router = express.Router();
+
+import somar from './src/helper';
+console.log(somar(1,2))    
+
 //Rotas
 const index = require('./src/routes');
 const personRoute = require('./src/routes/personRoute');
 app.use('/', index);
 app.use('/persons', personRoute);
+
 module.exports = app;
+
+
