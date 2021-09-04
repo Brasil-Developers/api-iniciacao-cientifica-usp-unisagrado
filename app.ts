260a372c42e79506c915e71a4cacb7b4c08100ac
Fabricio Carneiro Piscinelli <58093013+Fabricio-Carneiro-Piscinelli@users.noreply.github.com>
2021-09-03 23:16:06 -0300

    feat: [00008]: config redis

--- app.ts
+++ app.ts
@@ -1,7 +1,25 @@
 import express from 'express';
 
+import redis from 'redis';
+
 // Rotas
 import route from './src/routes';
+
+const cache = redis.createClient({
+  host: 'localhost',
+  port: 8081,
+  no_ready_check: true,
+});
+
+cache.on('connect', () => {
+  const getClient = cache.get('1');
+  console.log(getClient);
+//   global.console.log('connected');
+});
+
+cache.on('error', (err:any) => {
+  global.console.log(err.message);
+});
 
 const app = express();
 const versionAPI:string = 'v1';
