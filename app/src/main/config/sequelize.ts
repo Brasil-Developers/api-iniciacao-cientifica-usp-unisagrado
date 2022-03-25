0f39ab8966d928130fdd54b8cd17bd0276c3c1ab
Willian de Mattos Silva <willian_matt@hotmail.com>
2022-03-25 20:19:10 -0300

    feat: :rocket: Configure environment variables

--- app/src/main/config/sequelize.ts
+++ app/src/main/config/sequelize.ts
@@ -1,16 +1,16 @@
-import { Sequelize } from 'sequelize-typescript';
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 
 export default async () => {
     console.log('[DB] Iniciando conexão com o banco de dados...');
 
     const sequelize = new Sequelize({
-        dialect: 'mysql',
-        database: 'analise',
-        username: "root",
-        password: "root",
-        port: 3307,
+        dialect: process.env.DB_DIALECT || 'mysql',
+        database: process.env.DB_DATABASE || 'analise',
+        username: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "root",
+        port: process.env.DB_PORT || 3307,
         models: [__dirname + '/../../domain/models'],
-    });
+    } as SequelizeOptions);
 
     await sequelize.sync({ force: false });
     return sequelize;
