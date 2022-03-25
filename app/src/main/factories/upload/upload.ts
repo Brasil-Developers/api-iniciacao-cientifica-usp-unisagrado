44730a06f28748c394ad9d4d0d0d9e69f7415d4a
Willian de Mattos Silva <willian_matt@hotmail.com>
2022-03-25 19:59:52 -0300

    ref: :recycle: Refatorando repository Account

--- app/src/main/factories/upload/upload.ts
+++ app/src/main/factories/upload/upload.ts
@@ -1,13 +1,11 @@
 import { DbUploadData } from "../../../data/usecases/upload/db-upload-data";
 import { LogWinstonRepository } from "../../../infra/local/log-repository/log-repository";
-import { MulterUpload } from "../../../infra/local/upload/multer";
-import { UploadDataRepository } from "../../../infra/repositories/upload/upload-repository";
+import { UploadDataRepository } from "../../../infra/db/repositories/upload/upload-repository";
 import { UploadController } from "../../../presentation/controllers/upload/upload";
 import { Controller } from "../../../presentation/protocols";
 import { LogControllerDecorator } from "../../decoratos/logs";
 
 export const makeUploadController = (): Controller => {
-
     const uploadDataRepository = new UploadDataRepository();
     const dbUploadData = new DbUploadData(uploadDataRepository);
     const uploadController = new UploadController(dbUploadData);
