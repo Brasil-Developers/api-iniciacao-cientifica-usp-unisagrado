56815512c095650cfe0277b5e0c8feb10e54c545
Willian Mattos <willian_matt@hotmail.com>
2022-06-18 13:20:56 -0300

    feat: :sparkles: Add Speaker Route

--- app/src/main/middlewares/authentication/authentication.ts
+++ app/src/main/middlewares/authentication/authentication.ts
@@ -2,21 +2,31 @@
 import { NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { GenericError } from '../../../presentation/errors/generic-error';
-import { badRequest, serverError } from '../../../presentation/helpers/http-helper';
+import { Account } from '../../../domain/models/Account';
 import { HttpResponse } from '../../../presentation/protocols';
 
 export const verifyJWT = (req: any, res: any, next: NextFunction): HttpResponse | void => {
   try {
     const token = (req?.headers['x-access-token'] || req?.headers['authorization'] || null);
-  
+
     if (!token || token == null) throw new GenericError('No token provided.');
 
-    const decoded: any = jwt.verify(token.split(' ')[0], 'teste', (err: any, decoded: any) => {
+    const decoded: any = jwt.verify(token.split(' ')[0], 'teste', async (err: any, decoded: any) => {
       if (err) throw new GenericError('Invalid token.');
-      req.body.userId = decoded.id;
 
+      const account = await Account.findOne({
+        where: {
+          id: decoded.id,
+        },
+      })
+
+      if (!account) throw new GenericError('Invalid account token.');
+      if (new Date() > new Date(decoded.iat * 1000)) throw new GenericError('Token expired.');
+
+      req.body.userId = decoded.id;
       return next();
     });
+
     
   } catch (err: any) {
     console.log(err);
